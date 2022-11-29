acd9404ddcfa0f397d7792846ab6c8efc228ff67
maverick-ansh07 <anshmehta630@gmail.com>
2022-11-29 10:15:54 +0530

    29/11

---
+++ l4.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std ;
+ int main (){
+     int n ;
+     cout<<"enter n ";
+      cin >>n ;
+       int i=1,j=1 ;
+        
+      while (i<=n)
+      {
+        while (j<=n)
+        {
+           cout<<j<<j<<j;
+           
+        j++;
+cout<<endl;
+        }
+        
+        
+i++;
+
+      }
+      
+       return 0;
+        
+ }
