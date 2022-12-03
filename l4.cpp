2268b3745a95b8ccf02efa8b287a6bf4cb9e55ce
maverick-ansh07 <anshmehta630@gmail.com>
2022-12-03 12:04:58 +0530

    2/12

--- l4.cpp
+++ l4.cpp
@@ -1,26 +1,70 @@
 #include <iostream>
 using namespace std ;
- int main (){
-     int n ;
-     cout<<"enter n ";
-      cin >>n ;
-       int i=1,j=1 ;
-        
-      while (i<=n)
-      {
-        while (j<=n)
-        {
-           cout<<j<<j<<j;
-           
-        j++;
-cout<<endl;
-        }
-        
-        
-i++;
+ /*int main (){
+         int n ;
+          cin >>n;
+           int i=1;
+        //   int j =1 ; // j ka scope loop ke andar bhi to hina jaruri hai 
+             while (i<=n ){
+                  int j =1 ;
+               while (j<=n)
+               {
+                cout<<"*";
+                j++;
 
-      }
-      
-       return 0;
-        
+
+               }
+               cout<<endl;
+
+                 i++;
+                
+                  
+             }
+              return 1 ;
  }
+*/
+/*int main ()
+{
+int n ;
+ cin>>n;
+ int i =1;
+ while(i<=n){
+        int j =1;
+
+        while(j<=n){
+                cout<<i;
+                j++;
+
+        }
+cout<<endl;
+ i++;
+
+ }
+  return 0;
+   
+}*/
+int main (){
+         int n ;
+          cin >>n ;
+           int i =1 ;
+            while (i<=n ){
+
+                 int j =1;
+
+                //  while (j<=n){
+                       
+                //          cout<<j;
+                //           j++;
+                 while (j<=n){
+                       
+                         cout<<n-j+1;
+                          j++;
+
+                 }
+                 cout<<endl;
+                  i++;
+
+            }
+            return 1 ;
+             
+}
---
+++ l4c.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /*int n;
+     cin >>n ;
+     int i = 1 ;
+  
+      while (i<=n){
+        char c ='A';
+        int j = 1;
+        
+         while (j<=n)
+         {
+             cout<<c;
+             cout<<" ";
+              
+             c++;
+             j++;
+
+         }
+        
+         cout<<endl;
+         
+          i++;
+  
+    
+}return 0;
+
+
+A B C 
+A B C 
+A B C */
+
+
+/*int n;
+     cin >>n ;
+     int i = 1 ;
+     char c ='A';
+  
+      while (i<=n){
+        
+        int j = 1;
+        
+         while (j<=n)
+         {
+             cout<<c;
+             cout<<" ";
+              
+             c++;
+             j++;
+
+         }
+        
+         cout<<endl;
+         
+          i++;
+  
+    
+}return 0;
+*/
+// A B C 
+// D E F 
+// G H I 
+
+
+
+
+}
