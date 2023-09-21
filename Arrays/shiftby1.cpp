50664471fba0aada060845cc2e8b8d8ef9181f56
ansh-mehta7 <anshmehta630@gmail.com>
2023-09-21 10:18:11 +0530

    left shift right shift

--- Arrays/shiftby1.cpp
+++ Arrays/shiftby1.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-void shiftBy1(int arr[], int n){
+void leftshiftBy1(int arr[], int n){
     // last vali value ha usko temp me store karna hai 
     int temp=arr[n-1];
     // loop run karna h aur i me  i-1 vali value dalna hai 
@@ -11,6 +11,20 @@ void shiftBy1(int arr[], int n){
     // 0 index pe temp dalna hai 
     arr[0]=temp;
 }
+
+
+void rightshiftBy1(int arr[], int n){
+    // last vali value ha usko temp me store karna hai 
+    int temp=arr[0];
+    int i=0;
+    // loop run karna h aur i me  i-1 vali value dalna hai 
+    for (;i+1<n;i++){
+     arr[i]=arr[i+1];
+    }
+
+    // 0 index pe temp dalna hai 
+    arr[i]=temp;
+}
 void printArray(int arr[],int n){
  for (int i=0;i<n;i++){
     cout<<arr[i]<<" ";
@@ -19,7 +33,7 @@ void printArray(int arr[],int n){
 } 
 int main () {
      int arr[]={1,2,3,4,5,6};
-    shiftBy1(arr,6);
+    rightshiftBy1(arr,6);
     printArray(arr,6);
 return 0;
   }
