1e6ffebed2ffbe18f3b8e0e23a73cc3cc96633d9
ansh-mehta7 <anshmehta630@gmail.com>
2023-09-21 10:18:31 +0530

    pair sum

--- Arrays/twoSum.cpp
+++ Arrays/twoSum.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 
 void targetSum(int arr[],int n,int target){
-    for (int i=0;i<n;i++){
-        for (int j=i;j<n;j++){
+    for (int i=0;i<n/2;i++){
+        for (int j=i;j<n/2;j++){
             if (arr[i]+arr[j]==target){
                 cout<<arr[i]<<" "<<arr[j]<<endl;
             }
@@ -11,7 +11,7 @@ void targetSum(int arr[],int n,int target){
     }
 }
 int main () {
-    int arr[10]={1,2,3,4,5,6,7,8,9,10};
+    int arr[10]={0,1,2,3,4,5,6,7,8,9};
     int target ;
     cin>>target;
     targetSum(arr,10,target);
