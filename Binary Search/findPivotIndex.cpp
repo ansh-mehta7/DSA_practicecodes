669422f5023db9ce31188917b482d081d20eb08e
ansh-mehta7 <anshmehta630@gmail.com>
2023-09-29 00:34:15 +0530

    b search

--- Binary Search/findPivotIndex.cpp
+++ Binary Search/findPivotIndex.cpp
@@ -1,25 +1,26 @@
 #include <iostream>
 using namespace std;
 int findPivotIndex (int arr[], int n){
+
     int s=0;
-     int e=n-1;
-     int mid=s+(e-s)/2;
+    int e=n-1;
+    int mid=s+(e-s)/2;
     while (s<=e){
-        //  corner case 
+        //  corner case   
         if (s==e){
           return s;
         }
-        else if (arr[mid]>arr[mid+1]) return mid;
-        else if (arr[mid]<arr[mid-1]) return mid-1;
+        else if (mid+1<n && arr[mid]>arr[mid+1]) return mid;
+        else if (mid-1>=0 && arr[mid]<arr[mid-1]) return mid-1;
         else if (arr[s]>arr[mid]) e= mid-1;
         else s=mid+1;
 
-
+     mid=s+(e-s)/2;
     }
     return -1;
 }
 int main () {
-     int arr[]={12,14,16,18,4,6,8,10};
-     cout<<findPivotIndex(arr,8);
+     int arr[]={1,3};
+     cout<<findPivotIndex(arr,2);
 return 0;
   }
