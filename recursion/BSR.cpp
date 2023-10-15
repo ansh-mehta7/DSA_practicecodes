abb454c59b8dcc236361211bffa13c5b18af2c2e
ansh-mehta7 <anshmehta630@gmail.com>
2023-10-15 15:18:06 +0530

    recursion

---
+++ recursion/BSR.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+int bsr(int arr[],int s, int e ,int k){
+    // bc
+    if (s>e) return -1;
+    }
+
+// processing 
+int mid=s+(e-s)/2;
+if (arr[mid]==k) return mid;
+if (arr[mid]<k) return 
+if (arr[mid]>k)  return 
+
+
+using namespace std;
+int main () {
+    
+return 0;
+  }
